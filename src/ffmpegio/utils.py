53626bf29b22049b51cf54a82c1df73f8e0f576d
Takeshi Ikuma (LSUHSC) <tikuma@lsuhsc.edu>
2021-04-12 10:22:27 -0500

    added utils.spec_stream()

--- src/ffmpegio/utils.py
+++ src/ffmpegio/utils.py
@@ -3,11 +3,96 @@ import numpy as np
 from . import caps, probe
 
 
+def spec_stream(
+    index=None,
+    type=None,
+    program_id=None,
+    pid=None,
+    tag=None,
+    usable=None,
+    file_index=None,
+):
+    """Get stream specifier string
+
+    :param index: Matches the stream with this index. If stream_index is used as
+    an additional stream specifier, then it selects stream number stream_index
+    from the matching streams. Stream numbering is based on the order of the
+    streams as detected by libavformat except when a program ID is also
+    specified. In this case it is based on the ordering of the streams in the
+    program., defaults to None
+    :type index: int, optional
+    :param type: One of following: ’v’ or ’V’ for video, ’a’ for audio, ’s’ for
+    subtitle, ’d’ for data, and ’t’ for attachments. ’v’ matches all video
+    streams, ’V’ only matches video streams which are not attached pictures,
+    video thumbnails or cover arts. If additional stream specifier is used, then
+    it matches streams which both have this type and match the additional stream
+    specifier. Otherwise, it matches all streams of the specified type, defaults
+    to None
+    :type type: str, optional
+    :param program_id: Selects streams which are in the program with this id. If
+    additional_stream_specifier is used, then it matches streams which both are
+    part of the program and match the additional_stream_specifier, defaults to
+    None
+    :type program_id: int, optional
+    :param pid: stream id given by the container (e.g. PID in MPEG-TS
+    container), defaults to None
+    :type pid: str, optional
+    :param tag: metadata tag key having the specified value. If value is not
+    given, matches streams that contain the given tag with any value, defaults
+    to None
+    :type tag, str or tuple(key,value), optional
+    :param usable: streams with usable configuration, the codec must be defined
+    and the essential information such as video dimension or audio sample rate
+    must be present, defaults to None
+    :type usable: bool, optional
+    :param file_index: file index to be prepended if specified, defaults to None
+    :type file_index: int, optional
+    :return: stream specifier string or empty string if all arguments are None
+    :rtype: (fractions.Fraction, numpy.ndarray)
+
+    Note matching by metadata will only work properly for input files.
+
+    Note index, pid, tag, and usable are mutually exclusive. Only one of them
+    can be specified.
+
+    """
+
+    # nothing specified
+    if all(
+        [k is None for k in (index, type, program_id, pid, tag, usable, file_index)]
+    ):
+        return ""
+
+    spec = [] if file_index is None else [str(file_index)]
+
+    if program_id is not None:
+        spec.append(f"p:{program_id}")
+        
+    if type is not None:
+        spec.append(
+            dict(video="v", audio="a", subtitle="s", data="d", attachment="t").get(
+                type, type
+            )
+        )
+
+    if sum([k is not None for k in (index, pid, tag, usable)]) > 1:
+        raise Exception("Multiple mutually exclusive specifiers are given.")
+    if index is not None:
+        spec.append(str(index))
+    elif pid is not None:
+        spec.append(f"#{pid}")
+    elif tag is not None:
+        spec.append(f"m:{tag}" if isinstance(tag, str) else f"m:{tag[0]}:{tag[1]}")
+    elif usable is not None and usable:
+        spec.append("u")
+
+    return ":".join(spec)
+
+
 def config_image_reader(filename, index=0, **kwargs):
     info = probe.video_streams_basic(
         filename, index=index, entries=("width", "height", "pix_fmt")
     )[0]
-    print(info)
     width, height, input_pix_fmt = info.values()
 
     fmt_info = caps.pixfmts()[input_pix_fmt]
