c82f79ac0db1556492edfdcca7d5d5d624801a99
Takeshi Ikuma <tikuma@gmail.com>
2021-04-13 08:25:24 -0500

    added SimpleVideoReader

---
+++ src/ffmpegio/streams/__init__.py
@@ -0,0 +1,1 @@
+from .SimpleVideoReader import SimpleVideoReader
--- tests/test_probe.py
+++ tests/test_probe.py
@@ -10,7 +10,7 @@ import ffmpegio.probe as probe
 # )
 
 url = "tests/assets/testvideo-5m.mpg"
+print(probe.inquire(url, show_streams=('codec_type',)))
 print(probe.format_basic(url))
-
 print(probe.video_streams_basic(url))
 print(probe.audio_streams_basic(url))
