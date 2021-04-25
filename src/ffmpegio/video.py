5506b466148a477dea8c7ac6c4612c774257417e
Takeshi Ikuma (LSUHSC) <tikuma@lsuhsc.edu>
2021-04-25 15:50:32 -0500

    added video.create()

--- src/ffmpegio/video.py
+++ src/ffmpegio/video.py
@@ -1,5 +1,70 @@
 import numpy as np
-from . import ffmpeg, utils, configure
+from . import ffmpeg, utils, configure, filter_utils
+
+
+def create(name, *args, duration=1.0, **kwargs):
+    """Create a video using a source video filter
+
+    :param name: name of the source filter
+    :type name: str
+    :param \\*args: filter arguments
+    :type \\*args: tuple, optional
+    :param duration:
+    :type duration: int
+    :param \\**options: filter keyword arguments
+    :type \\**options: dict, optional
+    :return: video data
+    :rtype: numpy.ndarray
+
+    Supported Video Source Filters
+    ------------------------------
+
+    =============  ==============================================================================
+    filter name    description
+    =============  ==============================================================================
+    "color"        uniformly colored frame
+    "allrgb"       frames of size 4096x4096 of all rgb colors
+    "allyuv"       frames of size 4096x4096 of all yuv colors
+    "gradients"    several gradients
+    "mandelbrot"   Mandelbrot set fractal
+    "mptestsrc"    various test patterns of the MPlayer test filter
+    "life"         life pattern based on John Conway’s life game
+    "haldclutsrc"  identity Hald CLUT
+    "testsrc"      test video pattern, showing a color pattern
+    "testsrc2"     another test video pattern, showing a color pattern
+    "rgbtestsrc"   RGB test pattern useful for detecting RGB vs BGR issues
+    "smptebars"    color bars pattern, based on the SMPTE Engineering Guideline EG 1-1990
+    "smptehdbars"  color bars pattern, based on the SMPTE RP 219-2002
+    "pal100bars"   a color bars pattern, based on EBU PAL recommendations with 100% color levels
+    "pal75bars"    a color bars pattern, based on EBU PAL recommendations with 75% color levels
+    "yuvtestsrc"   YUV test pattern. You should see a y, cb and cr stripe from top to bottom
+    "sierpinski"   Sierpinski carpet/triangle fractal
+    =============  ==============================================================================
+
+    https://ffmpeg.org/ffmpeg-filters.html#Video-Sources
+
+    """
+
+    url = filter_utils.compose_filter(name, *args, **kwargs)
+
+    ffmpeg_args = configure.empty()
+    configure.add_url(ffmpeg_args, "input", url, {"f": "lavfi"})
+
+    ffmpeg_args, reader_cfg = configure.video_io(
+        ffmpeg_args,
+        url,
+        output_url="-",
+        format="rawvideo",
+    )
+    dtype, shape, rate = reader_cfg[0]
+
+    n = int(rate * duration)
+
+    configure.merge_user_options(
+        ffmpeg_args, "output", {"frames:v": n}, file_index=0
+    )
+    stdout = ffmpeg.run_sync(ffmpeg_args)
+    return np.frombuffer(stdout, dtype=dtype).reshape((-1, *shape))
 
 
 def read(url, vframes=None, stream_id=0, **options):
--- tests/test_video.py
+++ tests/test_video.py
@@ -2,6 +2,37 @@ from ffmpegio import video, probe
 import tempfile, re
 from os import path
 
+
+def test_create():
+    # make sure rgb channels are mapped properly
+    A = video.create("mandelbrot", r=25, size="320x240", duration=10.0)
+    assert A.shape == (250, 240, 320, 3)
+    # B = image.create("allrgb")
+    # B = image.create("allyuv")
+    # B = image.create("gradients=s=1920x1080:c0=000000:c1=434343:x0=0:x1=0:y0=0:y1=1080")
+    # B = image.create("gradients")
+    # B = image.create("mandelbrot")
+    # B = image.create("mptestsrc=t=dc_luma")
+    # B = image.create("mptestsrc", t="ring1")
+    # B = image.create("life",life_color='red')
+    # B = image.create("haldclutsrc",16)
+    # A = image.create("testsrc")
+    # B = image.create("testsrc2",alpha=0)
+    # B = image.create("rgbtestsrc")
+    # B = image.create("smptebars")
+    # B = image.create("smptehdbars")
+    # B = image.create("pal100bars")
+    # B = image.create("pal75bars")
+    # B = image.create("yuvtestsrc")
+    # B = image.create("sierpinski")
+    # print(A.dtype, A.shape)
+    # plt.subplot(1,2,1)
+    # plt.imshow(A)
+    # plt.subplot(1,2,2)
+    # plt.imshow(B)
+    # plt.show()
+
+
 def test_read_write():
     # url = "tests/assets/testvideo-5m.mpg"
     url = "tests/assets/testvideo-43.avi"
@@ -26,4 +57,3 @@ def test_read_write():
         fs, A = video.read(out_url, 10)
         print(fs)
         print(A.shape)
-        
