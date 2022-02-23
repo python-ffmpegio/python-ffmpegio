30339a06880833997854545bd872a84efbb597b2
Takeshi Ikuma (LSUHSC) <tikuma@lsuhsc.edu>
2022-02-22 18:42:10 -0600

    added function documentations

--- src/ffmpegio/path.py
+++ src/ffmpegio/path.py
@@ -27,6 +27,31 @@ def where():
 
 
 def find(ffmpeg_path=None, ffprobe_path=None):
+    """Set FFmpeg and FFprobe executables
+
+    :param ffmpeg_path: Full path to either the ffmpeg executable file or
+                        to the folder housing both ffmpeg and ffprobe, defaults to None
+    :type ffmpeg_path: str, optional
+    :param ffprobe_path: Full path to the ffprobe executable file, defaults to None
+    :type ffprobe_path: str, optional
+
+    If `ffmpeg_path` specifies a directory, the names of the executables are
+    auto-set to `ffmpeg` and `ffprobe`.
+
+    If the file locations are specified, the presence of the files will be
+    tested and an exception will be raised if both ffmpeg and ffprobe are not
+    valid executables.
+
+    If no argument is specified, the executables are auto-detected in the following orders.
+    (1) `ffmpeg` and `ffprobe` commands, i.e., the path to the parent directory
+        is included in the system PATH environmental variable.
+    (2) Run the `finder` plugin functions in the LIFO order and use the first valid
+        paths. There are two plugins currently offered: `ffmpegio-plugin-downloader`
+        and `ffmpegio-plugin-static-ffmpeg`.
+    (3) In Windows, additional locations are searched (e.g., C:\Program Files\ffmpeg).
+        See the documentation for the full list.
+
+    """
 
     global FFMPEG_BIN, FFPROBE_BIN
 
@@ -71,7 +96,13 @@ def find(ffmpeg_path=None, ffprobe_path=None):
 
 
 def get_ffmpeg(probe=False):
+    """Get the path to FFmpeg/FFprobe executable
 
+    :param probe: True to return FFprobe path instead, defaults to False
+    :type probe: bool, optional
+    :return: Path to FFmpeg/FFprobe exectutable
+    :rtype: str
+    """
     path = FFPROBE_BIN if probe else FFMPEG_BIN
 
     if not path:
