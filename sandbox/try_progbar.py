7c4c80673e433f65b2884d82a5145c89a927520b
Takeshi Ikuma (LSUHSC) <tikuma@lsuhsc.edu>
2022-02-23 22:10:12 -0600

    sandbox update

--- sandbox/try_progbar.py
+++ sandbox/try_progbar.py
@@ -29,6 +29,29 @@ vf = ffmpegio.FilterGraph(
     links={(2, 0, 0): (0, 0, 0), (2, 0, 1): (1, 1, 0)},
 )
 
+# vf = ffmpegio.FilterGraph(
+#     [
+#         [("color", {"c": "red", "s": "1280x30"})],
+#         [("color", {"c": "blue", "s": "1280x30"})],
+#         [("overlay", "(t/5-1)*main_w", 0, {"eval": "frame"})],
+#         [("overlay", 0, 720 - 30, {"shortest": 1})],
+#     ],
+#     input_labels={"0": (3, 0, 0)},
+#     output_labels={"out": (3, 0, 0)},
+#     links={(2, 0, 0): (0, 0, 0), (2, 0, 1): (1, 0, 0), (3, 0, 1): (2, 0, 0)},
+# )
+
+# vf = ffmpegio.FilterGraph(
+#     [
+#         [("drawbox", 0, 720 - 30, 1280, 30, "white", "fill")],
+#         [("color", {"c": "blue", "s": "1280x30"})],
+#         [("overlay", "(t/5-1)*w", 720 - 30, {"eval": "frame", "shortest": 1})],
+#     ],
+#     input_labels={"in": (0, 0, 0)},
+#     output_labels={"out": (2, 0, 0)},
+#     links={(2, 0, 0): (0, 0, 0), (2, 0, 1): (1, 0, 0)},
+# )
+
 print(vf)
 
 ffmpegio.transcode(
