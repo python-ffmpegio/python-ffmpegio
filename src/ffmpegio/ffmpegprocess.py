795f303a8ba8f68e6f9e2f3c759a1f42195aa6ec
Takeshi Ikuma (LSUHSC) <tikuma@lsuhsc.edu>
2022-03-06 09:47:32 -0600

    Turn on `-hwaccel auto` by default

--- src/ffmpegio/ffmpegprocess.py
+++ src/ffmpegio/ffmpegprocess.py
@@ -106,6 +106,22 @@ def exec(
         else:
             gopts["n"] = FLAG
 
+    # turn on hw decoder by default
+    def check_hwaccel(url, opts):
+        if opts is None:
+            opts = {"hwaccel": "auto"}
+        elif "hwaccel" not in opts:
+            opts["hwaccel"] = "auto"
+        return url, opts
+
+    if 'inputs' in ffmpeg_args:
+        try:
+            ffmpeg_args["inputs"] = [
+                check_hwaccel(url, opts) for url, opts in ffmpeg_args["inputs"]
+            ]
+        except:
+            pass
+
     # configure stdin pipe (if needed)
     def isreadable(f):
         try:
