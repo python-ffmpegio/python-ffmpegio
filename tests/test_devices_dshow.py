be635d036376d66fbf58b2542ec6aa9c95b2ab16
Takeshi Ikuma (LSUHSC) <tikuma@lsuhsc.edu>
2022-03-13 17:22:17 -0500

    Added version check

--- tests/test_devices_dshow.py
+++ tests/test_devices_dshow.py
@@ -1,11 +1,14 @@
 import logging, os
 from ffmpegio.plugins.devices import dshow
+from ffmpegio.path import FFMPEG_VER
+from packaging.version import Version
 import pytest
 
 
 @pytest.mark.skipif(os.name != "nt", reason="only run on windows")
 def test_dshow():
-    dshow._rescan()
+    if FFMPEG_VER < Version("5.0"):
+        dshow._rescan()
     devs = dshow._list_sources()
     for spec in devs.keys():
         dshow._resolve("source", spec)
@@ -18,10 +21,9 @@ if __name__ == "__main__":
 
     import ffmpegio
 
-    if ffmpegio.path.FFMPEG_VER!='5.0':
-        v5 = r"C:\Users\tikuma\AppData\Local\Programs\ffmpeg-5.0\bin"
-        ffmpegio.set_path(fr"{v5}\ffmpeg.exe", fr"{v5}\ffprobe.exe")
-        print(ffmpegio.path.FFMPEG_VER)
+    # if ffmpegio.path.FFMPEG_VER!='5.0':
+    #     v5 = r"C:\Users\tikuma\AppData\Local\Programs\ffmpeg-5.0\bin"
+    #     ffmpegio.set_path(fr"{v5}\ffmpeg.exe", fr"{v5}\ffprobe.exe")
+    #     print(ffmpegio.path.FFMPEG_VER)
 
     test_dshow()
-    
