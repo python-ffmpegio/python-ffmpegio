f66bc1885f2cb40f965150e4dc34c0ed5eec1a14
Takeshi Ikuma (LSUHSC) <tikuma@lsuhsc.edu>
2022-04-01 21:59:45 -0500

    test: updated dshow test

--- tests/test_devices_dshow.py
+++ tests/test_devices_dshow.py
@@ -1,19 +1,23 @@
 import logging, os
 from ffmpegio.plugins.devices import dshow
 from ffmpegio.path import FFMPEG_VER
+from ffmpegio import devices
 from packaging.version import Version
 import pytest
 
 
 @pytest.mark.skipif(os.name != "nt", reason="only run on windows")
 def test_dshow():
-    if FFMPEG_VER < Version("5.0"):
-        dshow._rescan()
-    devs = dshow._list_sources()
-    for spec in devs.keys():
-        dshow._resolve("source", spec)
-        dshow._list_options("source", spec)
-    dshow._resolve("source", "|".join(devs.keys()))
+    if FFMPEG_VER >= Version("5.0"):
+        devices.scan()
+        devs = devices.SOURCES["dshow"]["list"]
+    else:
+        devs = dshow._scan()
+    for _, spec in devs.items():
+        print(dshow._resolve([spec]))
+        print(dshow._list_options(spec))
+    if devs:
+        print(dshow._resolve([dev for dev in devs.values()]))
 
 
 if __name__ == "__main__":
@@ -26,4 +30,5 @@ if __name__ == "__main__":
     #     ffmpegio.set_path(fr"{v5}\ffmpeg.exe", fr"{v5}\ffprobe.exe")
     #     print(ffmpegio.path.FFMPEG_VER)
 
+    print(os.name)
     test_dshow()
