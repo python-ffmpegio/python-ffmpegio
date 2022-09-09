bd37cf5eb73341a743a3efb8f03667f9662a9fc6
Takeshi Ikuma (LSUHSC) <tikuma@lsuhsc.edu>
2022-09-09 09:10:14 -0500

    sandbox

--- sandbox/so73654626.py
+++ sandbox/so73654626.py
@@ -1,20 +1,15 @@
 import ffmpegio
 import subprocess as sp
 
-ffmpegio.ffmpeg("-y -f lavfi -i testsrc=size=640x480:rate=1:duration=10 sandbox/in.mp4")
+ffmpegio.ffmpeg(
+    "-y -f lavfi -i testsrc=size=640x480:rate=1:duration=100 sandbox/in.mp4"
+)
 
-p2 = ffmpegio.ffmpeg(
-    '-y -i sandbox/in.mp4 -f nut -i pipe: -filter_complex "[0:v][1:v]libvmaf=log_path=sandbox/log.json" -map 1 -c:v:1 copy sandbox/out.mp4',
-    sp_run=sp.Popen,
-    stdin=sp.PIPE,
-)
-p1 = ffmpegio.ffmpeg(
-    '-i sandbox/in.mp4 -vcodec libx264 -crf 27 -f nut pipe:"',
-    sp_run=sp.Popen,
-    stdout=p2.stdin,
-)
-p1.wait()
-try:
-    p2.wait(1)
-except:
-    p2.kill()
+cmd1 = "-loglevel quiet -i sandbox/in.mp4 -vcodec libx264 -crf 27 -f nut pipe:"
+cmd2 = '-loglevel debug -y -i sandbox/in.mp4 -f nut -i pipe: -filter_complex "[0:v][1:v]libvmaf=log_fmt=json:log_path=sandbox/log.json,nullsink" -map 1 -c:v:1 copy sandbox/out.mp4'
+
+ffmpeg = ffmpegio.get_path()
+fullcmd = f"{ffmpeg} {cmd1}|{ffmpeg} {cmd2}"
+sp.run(fullcmd, shell=True)
+
+print(f"ffmpeg {cmd1}|ffmpeg {cmd2}")
