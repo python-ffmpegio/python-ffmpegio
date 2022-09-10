fb7b21cc83a499d80b7f374fa29e5edd6ab2ce96
Takeshi Ikuma (LSUHSC) <tikuma@lsuhsc.edu>
2022-09-10 09:39:45 -0500

    cleared up sandbox

--- sandbox/try_ffprobe.py
+++
@@ -1,6 +0,0 @@
-from ffmpegio import ffmpeg, probe
-
-# url = "tests/assets/testaudio-1m.mp3"
-url = "tests/assets/testmulti-1m.mp4"
-
-print(probe.full_details(url,select_streams=[2]))
--- sandbox/try_hw_device.py
+++
@@ -1,5 +0,0 @@
-from ffmpegio import ffmpegprocess as ffprocess
-
-print(ffprocess.versions())
-
-ffprocess.run({"global_options": {"init_hw_device": "cuda"}})
