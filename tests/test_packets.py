4846cda312158c8ec2bbc9219f506cf64ece10c2
Takeshi Ikuma (LSUHSC) <tikuma@lsuhsc.edu>
2022-09-14 21:11:25 -0500

    wip

---
+++ tests/test_packets.py
@@ -0,0 +1,6 @@
+if __name__=='__main__":
+    # packet reader module
+    # - mp4/mov libavformat\mov.c::static int mov_read_default(MOVContext *c, AVIOContext *pb, MOVAtom atom)
+    #   av_log(c->fc, AV_LOG_TRACE, "type:'%s' parent:'%s' sz: %"PRId64" %"PRId64" %"PRId64"\n", 
+    #          av_fourcc2str(a.type), av_fourcc2str(atom.type), a.size, total_size, atom.size);
+    pass
