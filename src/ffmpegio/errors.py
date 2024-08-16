b7d0282d14dae3017907e6b0e85013c8c9dcbb2e
Takeshi Ikuma (LSUHSC) <tikuma@lsuhsc.edu>
2024-08-16 12:06:48 -0500

    added an ff5 error message to scan_stderr

--- src/ffmpegio/errors.py
+++ src/ffmpegio/errors.py
@@ -283,6 +283,8 @@ def scan_stderr(logs: Union[str, Sequence[str], None]):
             msg = (
                 f"{logs[-2]}\n  {msg0}" if logs[-2].startswith("[lavfi ") else logs[-2]
             )  # ...
+        elif msg0.endswith('No such file or directory'):
+            msg = msg0
     return msg
 
 
