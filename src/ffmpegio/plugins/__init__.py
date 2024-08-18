9a2e0c87f5a98597f871139d134529514f031ef7
Takeshi Ikuma (LSUHSC) <tikuma@lsuhsc.edu>
2024-08-18 12:48:49 -0500

    wip

--- src/ffmpegio/plugins/__init__.py
+++ src/ffmpegio/plugins/__init__.py
@@ -5,9 +5,12 @@ import logging
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
-import pluggy, os
+from typing import Literal
 
 from importlib import import_module
+import re, os
+import pluggy
+
 
 from . import hookspecs
 
@@ -17,7 +20,7 @@ pm = pluggy.PluginManager("ffmpegio")
 pm.add_hookspecs(hookspecs)
 
 
-def _try_register_builtin(module_name: str) -> str | None:
+def _try_register_builtin(module_name: str, reregister: bool = False) -> str | None:
     try:
         module = import_module(f".{module_name}", "ffmpegio.plugins")
     except:
@@ -26,14 +29,62 @@ def _try_register_builtin(module_name: str) -> str | None:
         )
     else:
         logger.info(f"registered {module_name} builtin-plugin module")
+        if reregister:
+            pm.unregister(module)
         return pm.register(module)
 
 
-def initialize():
-    from . import rawdata_bytes
+def register(plugin: object, name: str | None = None) -> str | None:
+    """Register a plugin and return its name.
 
-    # load bundled base plugins
-    pm.register(rawdata_bytes)
+    :param plugin: Plugin object
+    :param name: The name under which to register the plugin. If not specified,
+                 a name is generated using get_canonical_name().
+    :returns: The plugin name. If the name is blocked from registering, returns None.
+
+    If the plugin is already registered, raises a ValueError.
+    """
+    return pm.register(plugin, name)
+
+
+def use(name: Literal["read_numpy", "read_bytes"] | str):
+    """Select the plugin to use (among contentios plugins)
+
+    :param name: The plugin to switch to. This can either be ``'read_numpy'`` or
+                 ``'read_bytes'`` or a plugin module name:
+
+                 - ``"read_numpy"`` - All the media readers to output a Numpy array.
+                               Also, reverts Numpy array input processing by
+                               all the writers to the default. Numpy must be
+                               installed for this plugin to be activated.
+                 - ``"read_bytes"`` - All the media readers to output a dict with keys:
+                                    ``"buffer"`` (``bytes``) of the retrieved data,
+                                    ``"dtype"`` (``str``) Numpy dtype string of ``'"buffer"'``,
+                                    and ``"shape"`` (``tuple`` of ``int``s) the data array shape
+                                    of ``'"buffer"'``
+
+                 If a plugin name is given, it must be in the form:
+                 `plugin://my.plugin.name`.
+    """
+
+    if name == "numpy":
+        _try_register_builtin("rawdata_numpy", True)
+    elif name == "rawbytes":
+        _try_register_builtin("rawdata_bytes", True)
+    else:
+        matched_name = re.match(r"plugin://(.+)$", name)
+        if matched_name is None:
+            raise ValueError(f'{name=} must follow "plugin://my.plugin.name" format')
+        plugin = pm.get_plugin(matched_name)
+        if plugin is None:
+            raise ValueError(f"Requested plugin ({name=}) has not been registered")
+        pm.unregister(plugin)
+        pm.register(plugin)
+
+
+def initialize():
+    """initilaize manager and load builtin plugins"""
+
     if os.name == "nt":
         for name in ["finder_win32"]:
             _try_register_builtin(name)
@@ -42,7 +93,7 @@ def initialize():
 
         pm.register(dshow)
 
-    for name in ["finder_ffdl", "mpl_writer", "numpy_data"]:
+    for name in ["rawdata_bytes", "finder_ffdl", "rawdata_mpl", "rawdata_numpy"]:
         _try_register_builtin(name)
 
     # load all ffmpegio plugins found in site-packages
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,12 @@
 import pytest
 
-from ffmpegio.plugins import pm, rawdata_bytes, finder_win32
+from ffmpegio.plugins import pm, rawdata_bytes, finder_win32, finder_ffdl
 
 # test only with the base plugins
 @pytest.fixture(scope="session", autouse=True)
 def no_extra_plugins():
-    base_plugins = (rawdata_bytes, finder_win32)
-    plugins = [p for p in pm.get_plugins() if p not in base_plugins and pm.get_name(p)!='finder_downloader']
+    base_plugins = (rawdata_bytes, finder_win32, finder_ffdl)
+    plugins = [p for p in pm.get_plugins() if p not in base_plugins]
     for p in plugins:
         pm.unregister(p)
     yield
