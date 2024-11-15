eed17ea138e07755486afc91dc96a94324f431b5
Takeshi Ikuma (LSUHSC) <tikuma@lsuhsc.edu>
2024-11-14 21:50:46 -0600

    update

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,16 @@
 import pytest
 
-from ffmpegio.plugins import pm, rawdata_bytes, finder_win32, finder_ffdl
+import ffmpegio as ff
+
 
 # test only with the base plugins
 @pytest.fixture(scope="session", autouse=True)
 def no_extra_plugins():
-    base_plugins = (rawdata_bytes, finder_win32, finder_ffdl)
-    plugins = [p for p in pm.get_plugins() if p not in base_plugins]
-    for p in plugins:
-        pm.unregister(p)
-    yield
-    for p in reversed(plugins):
-        pm.register(p)
+    ff.use("read_bytes")
+    # base_plugins = (rawdata_bytes, finder_win32, finder_ffdl)
+    # plugins = [p for p in pm.get_plugins() if p not in base_plugins]
+    # for p in plugins:
+    #     pm.unregister(p)
+    # yield
+    # for p in reversed(plugins):
+    #     pm.register(p)
