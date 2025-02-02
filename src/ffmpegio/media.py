37d09fbb164fbdd39643be7f59ad8147c475c8cd
Takeshi Ikuma (LSUHSC) <tikuma@lsuhsc.edu>
2025-02-01 19:29:18 -0600

    Added media.write() function

--- src/ffmpegio/media.py
+++ src/ffmpegio/media.py
@@ -1,9 +1,23 @@
+from __future__ import annotations
+
+from typing_extensions import Unpack
+from collections.abc import Sequence
+from .utils.typing import Literal, Any, RawStreamDef, ProgressCallable
+
+import contextlib
 from io import BytesIO
+from fractions import Fraction
 
-from . import ffmpegprocess, utils, configure, FFmpegError
+from namedpipe import NPopen
+
+from .threading import WriterThread
+from .filtergraph.presets import merge_audio
+
+from . import ffmpegprocess, utils, configure, FFmpegError, plugins
 from .utils import avi
+from .threading import WriterThread
 
-__all__ = ["read"]
+__all__ = ["read", "write"]
 
 
 def read(*urls, progress=None, show_log=None, **options):
@@ -91,3 +105,191 @@ def read(*urls, progress=None, show_log=None, **options):
     }
 
     return rates, data
+
+
+def write(
+    url: str,
+    stream_types: Sequence[Literal["a", "v"]],
+    *stream_args: * tuple[RawStreamDef, ...],
+    merge_audio_streams: bool | Sequence[int] = False,
+    merge_audio_ar: int | None = None,
+    merge_audio_sample_fmt: str | None = None,
+    merge_audio_outpad: str | None = None,
+    progress: ProgressCallable | None = None,
+    overwrite: bool | None = None,
+    show_log: bool | None = None,
+    extra_inputs: Sequence[str | tuple[str, dict]] | None = None,
+    sp_kwargs: dict | None = None,
+    **options: Unpack[dict[str, Any]],
+):
+    """write multiple streams to a url/file
+
+    :param url: output url
+    :stream_types: list/string of input stream media types, each element is either 'a' (audio) or 'v' (video)
+    :param stream_args: raw input stream data arguments, each input stream is either a tuple of a sample rate (audio) or frame rate (video) followed by a data blob
+                         or a tuple of a data blob and a dict of input options. The option dict must include `'ar'` (audio) or `'r'` (video) to specify the rate.
+    :param merge_audio_streams: True to combine all input audio streams as a single multi-channel stream. Specify a list of the input stream id's
+                                (indices of `stream_types`) to combine only specified streams.
+    :param merge_audio_ar: Sampling rate of the merged audio stream in samples/second, defaults to None to use the sampling rate of the first merging stream
+    :param merge_audio_sample_fmt: Sample format of the merged audio stream, defaults to None to use the sample format of the first merging stream
+    :param progress: progress callback function, defaults to None
+    :param overwrite: True to overwrite if output url exists, defaults to None (auto-select)
+    :param show_log: True to show FFmpeg log messages on the console, defaults to None (no show/capture)
+    :param extra_inputs: list of additional input sources, defaults to None. Each source may be url
+                         string or a pair of a url string and an option dict.
+    :param sp_kwargs: dictionary with keywords passed to `subprocess.run()` or `subprocess.Popen()` call
+                      used to run the FFmpeg, defaults to None
+    :param **options: FFmpeg options, append '_in' for input option names (see :doc:`options`). Input options
+                      will be applied to all input streams unless the option has been already defined in `stream_data`
+
+    TIPS
+    ----
+
+    * All the input streams will be added to the output file by default, unless `map` option is specified
+    * If the input streams are of different durations, use `shortest=ffmpegio.FLAG` option to trim all streams to the shortest.
+    * Using merge_audio_streams:
+      - adds a `filter_complex` global option
+      - merged input streams are removed from the `map` option and replaced by the merged stream
+
+    """
+
+    if not all(t in "av" for t in stream_types):
+        raise ValueError("Elements of stream_types input must either 'a' or 'v'.")
+
+    # analyze input stream_data
+    n_in = len(stream_types)
+
+    if len(stream_args) != n_in:
+        raise ValueError(f"Lengths of `stream_args` and `stream_types` not matching.")
+
+    # separate the input options from the rest of the options
+    default_in_opts = utils.pop_extra_options(options, "_in")
+
+    url, stdout, _ = configure.check_url(url, True)
+
+    # create FFmpeg argument dict
+    ffmpeg_args = configure.empty()
+
+    # add input streams
+    pipes = []  # named pipes and their data blobs (one for each input stream)
+
+    for mtype, arg in zip(stream_types, stream_args):
+
+        try:
+            a1, a2 = arg
+            if isinstance(a1, (int, float, Fraction)):
+                opts, data = a1, a2
+            else:
+                assert isinstance(a2, dict)
+                data, opts = a1, a2
+        except:
+            raise ValueError(
+                f"""Invalid raw stream definition: {arg}.\nEach item of `stream_args` must be a two-element tuple: 
+                    - a rate (numeric) and a data_blob
+                    - a data_blob and a dict of options
+                """
+            )
+
+        pipe = NPopen("w", bufsize=0)
+
+        if mtype == "a":  # audio
+            if not isinstance(opts, dict):
+                opts = {"ar": round(opts)}
+            elif "ar" not in opts:
+                raise ValueError(
+                    "audio stream option dict missing the required 'ar' item to set the sampling rate."
+                )
+            in_args = configure.array_to_audio_input(
+                pipe_id=pipe.path, data=data, **{**default_in_opts, **opts}
+            )
+            byte_data = plugins.get_hook().audio_bytes(obj=data)
+
+        else:  # video
+            if not isinstance(opts, dict):
+                opts = {"r": opts}
+            elif "r" not in opts:
+                raise ValueError(
+                    "video stream option dict missing the required 'r' item to set the frame rate."
+                )
+            in_args = configure.array_to_video_input(
+                pipe_id=pipe.path, data=data, **{**default_in_opts, **opts}
+            )
+            byte_data = plugins.get_hook().video_bytes(obj=data)
+
+        pipes.append((pipe, byte_data))
+
+        configure.add_url(
+            ffmpeg_args,
+            "input",
+            *in_args,
+        )
+
+    # map all input streams to output unless user specifies the mapping
+    map = options["map"] if "map" in options else list(range(n_in))
+    do_merge = bool(merge_audio_streams) and stream_types.count("a") > 1
+    if do_merge:
+        if merge_audio_streams is True:
+            # if True, convert to stream indices of audio inputs
+            merge_audio_streams = [
+                i for i, mtype in enumerate(stream_types) if mtype == "a"
+            ]
+        else:
+            try:
+                assert all(stream_types[i] == "a" for i in merge_audio_streams)
+            except AssertionError:
+                raise ValueError(
+                    "merge_audio_streams argument must be bool or a sequence of indices of input audio streams."
+                )
+
+        # assign the final map - exclude audio streams if to be merged together
+        options["map"] = [i for i in map if i not in merge_audio_streams]
+
+    # add output url and options (may also contain possibly global options)
+    configure.add_url(ffmpeg_args, "output", url, options)
+
+    # add extra input arguments if given
+    if extra_inputs is not None:
+        configure.add_urls(ffmpeg_args, "input", extra_inputs)
+
+    if do_merge:
+
+        # get FFmpeg input list
+        ffinputs = ffmpeg_args["inputs"]
+        audio_streams = {i: ffinputs[i][1] for i in merge_audio_streams}
+        afilt = merge_audio(
+            audio_streams,
+            merge_audio_ar,
+            merge_audio_sample_fmt,
+            merge_audio_outpad or "aout",
+        )
+
+        # add the merging filter graph to the filter_complex argument
+        configure.add_filtergraph(ffmpeg_args, afilt)
+
+    kwargs = {**sp_kwargs} if sp_kwargs else {}
+    kwargs.update(
+        {
+            "stdout": stdout,
+            "progress": progress,
+            "overwrite": overwrite,
+        }
+    )
+    kwargs["capture_log"] = None if show_log else False
+
+    with contextlib.ExitStack() as stack:
+        # run the FFmpeg
+        proc = ffmpegprocess.Popen(ffmpeg_args, **kwargs)
+
+        # connect the pipes and queue the stream data
+        for p, data in pipes:
+            stack.enter_context(p)
+            writer = WriterThread(p)
+            stack.enter_context(writer)
+            writer.write(data)  # send bytes in out_bytes to the client
+            writer.write(None)  # sentinel message
+
+        # wait for the FFmpeg to finish processing
+        proc.wait()
+
+    if proc.returncode:
+        raise FFmpegError(proc.stderr, show_log)
--- tests/test_media.py
+++ tests/test_media.py
@@ -1,52 +1,60 @@
-from ffmpegio import media
+from tempfile import TemporaryDirectory
+from os import path
+from pprint import pprint
+
+import ffmpegio as ff
 
 
 def test_media_read():
     url = "tests/assets/testmulti-1m.mp4"
     url1 = "tests/assets/testvideo-1m.mp4"
     url2 = "tests/assets/testaudio-1m.mp3"
-    rates, data = media.read(url, t=1)
-    rates, data = media.read(url, map=("v:0", "v:1", "a:1", "a:0"), t=1)
-    rates, data = media.read(url1, url2, t=1)
-    rates, data = media.read(url2, url, map=("1:v:0", (0, "a:0")), t=1)
+    rates, data = ff.media.read(url, t=1)
+    rates, data = ff.media.read(url, map=("v:0", "v:1", "a:1", "a:0"), t=1)
+    rates, data = ff.media.read(url1, url2, t=1)
+    rates, data = ff.media.read(url2, url, map=("1:v:0", (0, "a:0")), t=1)
 
     print(rates)
-    print([(k, x['shape'], x['dtype']) for k, x in data.items()])
+    print([(k, x["shape"], x["dtype"]) for k, x in data.items()])
 
 
-if __name__ == "__main__":
-    from matplotlib import pyplot as plt
+def test_media_write():
+    fs, x = ff.audio.read("tests/assets/testaudio-1m.mp3")
 
-    pass
-    # out = ffmpegprocess.run(
-    #     {
-    #         "inputs": [(url, None)],
-    #         "outputs": [
-    #             (
-    #                 "-",
-    #                 {
-    #                     "ss": 0.1,
-    #                     "t": 1,
-    #                     "f": "avi",
-    #                     "c:v": "rawvideo",
-    #                     "pix_fmt": "ya8",
-    #                     "c:a": "pcm_f32le",
-    #                     "sample_fmt": "flt",
-    #                 },
-    #             )
-    #         ],
-    #         "global_options": None,
-    #     },
-    #     capture_log=False,
-    # )
-    # reader = avi.AviReader(BytesIO(out.stdout), True)
-    # print(reader.streams)
-    # n = len(reader.streams)
-    # out = {v["spec"]: [] for v in reader.streams.values()}
-    # for st, data in reader:
-    #     out[st].append(data)
-    # out = {k: np.concatenate(v) for k, v in out.items()}
-    # print({k: (v.shape, v.dtype) for k, v in out.items()})
+    fps, F = ff.video.read("tests/assets/testvideo-1m.mp4", vframes=120)
 
-    # plt.imshow(out["v:0"][0, ..., 0], alpha=out["v:0"][0, ..., 1]/255)
-    # plt.show()
+    outext = ".mp4"
+
+    print(f"video: {len(F['buffer'])} bytes | audio: {len(x['buffer'])} bytes")
+
+    with TemporaryDirectory() as tmpdirname:
+        outfile = path.join(tmpdirname, f"out{outext}")
+        ff.media.write(
+            outfile, "va", (fps, F), (fs, x), show_log=True, shortest=ff.FLAG
+        )
+
+        pprint(ff.probe.format_basic(outfile))
+        pprint(ff.probe.streams_basic(outfile))
+
+
+def test_media_write_audio_merge():
+    stream1 = ff.audio.read("tests/assets/testaudio-1m.mp3", ar=8000, sample_fmt="s16")
+    stream2 = ff.audio.read("tests/assets/testaudio-1m.mp3", ar=16000, sample_fmt="flt")
+    stream3 = ff.audio.read("tests/assets/testaudio-1m.mp3", ar=4000, sample_fmt="dbl")
+
+    outext = ".wav"
+
+    with TemporaryDirectory() as tmpdirname:
+        outfile = path.join(tmpdirname, f"out{outext}")
+        ff.media.write(
+            outfile,
+            "aaa",
+            stream1,
+            stream2,
+            stream3,
+            merge_audio_streams=True,
+            show_log=True,
+            shortest=ff.FLAG,
+        )
+        pprint(ff.probe.format_basic(outfile))
+        pprint(ff.probe.audio_streams_basic(outfile))
