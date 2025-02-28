1fb423d8743d44dd2c86640a56ed4568fdf90ec8
Takeshi Ikuma <tikuma@gmail.com>
2025-02-28 10:34:19 -0600

    added `PipedMediaWriter` class

--- tests/test_pipedstreams.py
+++ tests/test_pipedstreams.py
@@ -4,6 +4,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 from os import path
 import pytest
+from tempfile import TemporaryDirectory
 
 import ffmpegio as ff
 from ffmpegio import streams
@@ -20,3 +21,42 @@ def test_PipedMediaReader():
         for data in reader:
             for k, v in data.items():
                 print(f"{k}: {len(v['buffer'])}")
+
+
+def test_PipedMediaWriter_audio():
+
+    ff.use('read_numpy')
+
+    rates, data = ff.media.read(audio_url, t=1,ar=8000,sample_fmt='s16')
+    stream_types = [spec.split(":", 2)[1] for spec in data]
+
+    with streams.PipedMediaWriter(
+        "pipe", stream_types, *rates.values(), show_log=True, f="matroska", loglevel="debug"
+    ) as writer:
+        for i, (mtype, frame) in enumerate(zip(stream_types, data.values())):
+            writer.write(i, frame)
+            writer.write(i, None)
+
+        writer.wait(1)
+        b = writer.pop_encoded()
+
+
+def test_PipedMediaWriter():
+
+    ff.use('read_numpy')
+
+    rates, data = ff.media.read(mult_url, t=1)
+    stream_types = [spec.split(":", 2)[1] for spec in data]
+
+    with streams.PipedMediaWriter(
+        "pipe", stream_types, *rates.values(), show_log=True, f="matroska", loglevel="debug"
+    ) as writer:
+        for i, (mtype, frame) in enumerate(zip(stream_types, data.values())):
+            if mtype == "v":
+                writer.write(i, frame[0])
+            else:
+                writer.write(i, frame[0:100])
+
+        writer.wait(1)
+        b = writer.pop_encoded(0)
+        assert isinstance(b,bytes)
