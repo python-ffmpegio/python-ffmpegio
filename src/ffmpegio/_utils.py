8c1caab4ae801deb8778262f733cc7a8575a467f
Takeshi Ikuma <tikuma@gmail.com>
2025-03-05 12:34:56 -0600

    `is_pipe()` includes "pipe"

--- src/ffmpegio/_utils.py
+++ src/ffmpegio/_utils.py
@@ -133,7 +133,7 @@ def is_url(value: Any, *, pipe_ok: bool = False) -> bool:
 
 def is_pipe(value: Any) -> bool:
     """True if FFmpeg pipe protocol string"""
-    return value == "-" or bool(re.match(r"pipe\:\d*", value))
+    return value == "-" or bool(re.match(r"pipe(\:\d*)?", value))
 
 
 def is_namedpipe(
