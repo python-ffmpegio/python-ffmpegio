4fddbe00b416356599a45954cfa6a682419f287e
Takeshi Ikuma (LSUHSC) <tikuma@lsuhsc.edu>
2025-03-08 22:17:34 -0600

    - `transcode()` to form its FFmpeg arguments by `init_media_transcoder()` - added type hints

--- src/ffmpegio/transcode.py
+++ src/ffmpegio/transcode.py
@@ -1,53 +1,74 @@
+from __future__ import annotations
+
+import logging
+
+logger = logging.getLogger("ffmpegio")
+
+from ._typing import Sequence, ProgressCallable, Unpack
+from .configure import (
+    FFmpegOutputUrlComposite,
+    FFmpegInputUrlComposite,
+    FFmpegOptionDict,
+    FFmpegInputOptionTuple,
+    FFmpegOutputOptionTuple,
+)
+
+
 from . import ffmpegprocess as fp, configure, utils, FFmpegError
 from .path import check_version
-from .errors import scan_stderr
 
 __all__ = ["transcode"]
 
 
 def transcode(
-    inputs,
-    outputs,
-    progress=None,
-    overwrite=None,
-    show_log=None,
-    two_pass=False,
-    pass1_omits=None,
-    pass1_extras=None,
-    sp_kwargs=None,
-    **options,
-):
+    inputs: (
+        FFmpegInputUrlComposite
+        | Sequence[FFmpegInputUrlComposite | FFmpegInputOptionTuple]
+    ),
+    outputs: (
+        FFmpegOutputUrlComposite
+        | Sequence[FFmpegOutputUrlComposite | FFmpegOutputOptionTuple]
+    ),
+    *,
+    progress: ProgressCallable | None = None,
+    overwrite: bool | None = None,
+    show_log: bool | None = None,
+    two_pass: bool = False,
+    pass1_omits: (
+        Sequence[str] | Sequence[Sequence[str]] | dict[int, Sequence[str]] | None
+    ) = None,
+    pass1_extras: (
+        FFmpegOptionDict
+        | Sequence[FFmpegOptionDict]
+        | dict[int, FFmpegOptionDict]
+        | None
+    ) = None,
+    sp_kwargs: dict | None = None,
+    **options: Unpack[FFmpegOptionDict],
+) -> bytes | None:
     """Transcode media files to another format/encoding
 
     :param inputs: url/path of the input media file or a sequence of tuples, each
                    containing an input url and its options dict
-    :type inputs: str or a list of str or a sequence of (str,dict)
     :param outputs: url/path of the output media file or a sequence of tuples, each
                     containing an output url and its options dict
-    :type outputs: str or sequence of (str, dict)
     :param progress: progress callback function, defaults to None
-    :type progress: callable object, optional
     :param overwrite: True to overwrite if output url exists, defaults to None
                       (auto-select)
-    :type overwrite: bool, optional
     :param show_log: True to show FFmpeg log messages on the console,
                      defaults to None (no show/capture)
                      Ignored if stream format must be retrieved automatically.
-    :type show_log: bool, optional
     :param two_pass: True to encode in 2-pass
     :param pass1_omits: list of output arguments to ignore in pass 1, defaults to
                         None (removes 'c:a' or 'acodec'). For multiple outputs,
                         specify use list of the list of arguments, matching the
                         length of outputs, for per-output omission.
-    :type pass1_omits: seq(str), or seq(seq(str)) optional
     :param pass1_extras: list of additional output arguments to include in pass 1,
                          defaults to None (add 'an' if `pass1_omits` also None)
-    :type pass1_extras: dict(int:dict(str)), optional
     :param sp_kwargs: dictionary with keywords passed to `subprocess.run()` or
                       `subprocess.Popen()` call used to run the FFmpeg, defaults
                       to None
-    :type sp_kwargs: dict, optional
-    :param \\**options: FFmpeg options. For output and global options, use FFmpeg
+    :param **options: FFmpeg options. For output and global options, use FFmpeg
                         option names as is. For input options, append "_in" to the
                         option name. For example, r_in=2000 to force the input frame
                         rate to 2000 frames/s (see :doc:`options`).
@@ -57,53 +78,49 @@ def transcode(
                         options, and the url-specific duplicate options in the
                         ``inputs`` or ``outputs`` sequence will overwrite those
                         specified here.
-    :type \\**options: dict, optional
     :returns: if any of the outputs is stdout, returns output bytes
-    :rtype: bytes | None
 
     """
 
-    # split input and global options from options
-    input_options = utils.pop_extra_options(options, "_in")
-    global_options = utils.pop_global_options(options)
+    if utils.is_valid_input_url(inputs):
+        inputs = [inputs]
+    if utils.is_valid_output_url(outputs):
+        outputs = [outputs]
 
-    def format_arg(arg, defopts):
-        def test(a, is_list):
-            try:
-                assert len(a) == 2
-                assert isinstance(a[1], dict)
-                return (a[0], {**defopts, **a[1]})
-            except:
-                if is_list:
-                    return (a, defopts)
-                raise
+    args, input_info, output_info = configure.init_media_transcoder(
+        inputs, outputs, None, None, options
+    )
 
-        # special case: a list of inputs w/out options
-        if type(arg) == list:
-            return [test(a, True) for a in arg]
+    # check number of pipes
+    nb_inpipes = sum(info["src_type"] == "buffer" for info in input_info)
+    nb_outpipes = sum(info["dst_type"] == "buffer" for info in output_info)
 
-        # attempt to map url-options pairs
-        try:
-            return [test(a, False) for a in arg]
-        except:
-            return [(arg, defopts)]
+    # if 0 or 1 buffered input and 0 or 1 buffered output, just use stdin/stdout
+    simple_mode = nb_inpipes < 2 and nb_outpipes < 2
 
-    inputs = format_arg(inputs, input_options)
-    outputs = format_arg(outputs, options)
+    if not simple_mode:
+        raise NotImplementedError(
+            "transcoding with multiple input or output pipes is not yet implemented."
+        )
 
-    # initialize FFmpeg argument dict
-    args = configure.empty(global_options)
-
-    for url, opts in inputs:
-        input_url, stdin, input = configure.check_url(url, False, opts.get("f", None))
-        configure.add_url(args, "input", input_url, opts)
-
-    for url, opts in outputs:
-        output_url, stdout, _ = configure.check_url(url, True)
-        i, _ = configure.add_url(args, "output", output_url, opts)
-
-        # convert basic VF options to vf option
+    # convert basic VF options to vf option
+    for i in range(len(output_info)):
         configure.build_basic_vf(args, None, i)
+
+    stdin = stdout = input = None
+    if nb_inpipes:
+        inputs = args["inputs"]
+        for i, info in enumerate(input_info):
+            inputs[i] = ("pipe:0", inputs[i][1])
+            if "buffer" in info:
+                input = info["buffer"]
+            else:
+                stdin = fp.PIPE
+    if nb_outpipes:
+        outputs = args["outputs"]
+        for i, info in enumerate(output_info):
+            outputs[i] = ("pipe:1", outputs[i][1])
+            stdout = fp.PIPE
 
     kwargs = {**sp_kwargs} if sp_kwargs else {}
     kwargs.update(
