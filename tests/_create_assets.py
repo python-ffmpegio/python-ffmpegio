8598566f866b4c105c11a0e76c4b4359a101b5c7
Takeshi Ikuma (LSUHSC) <tikuma@lsuhsc.edu>
2026-07-05 12:34:15 -0500

    updated asset generation (setting overwrite option globally)

--- tests/_create_assets.py
+++ tests/_create_assets.py
@@ -1,18 +1,18 @@
-from ffmpegio import ffmpegprocess, probe
 from os import path
 from pprint import pprint
 
+from ffmpegio import ffmpegprocess, probe
 
 command_list = (
     {
         "inputs": [("testsrc=r=30000/1001:d=60", {"f": "lavfi"})],
         "outputs": [("tests/assets/testvideo-1m.mp4", None)],
-        "global_options": {"n": None},
+        "global_options": {},
     },
     {
         "inputs": [("testsrc=r=30000/1001:d=60", {"f": "lavfi"})],
         "outputs": [("tests/assets/testvideo-1m-lowres.mp4", {"crf": 25})],
-        "global_options": {"n": None},
+        "global_options": {},
     },
     {
         "inputs": [
@@ -22,7 +22,7 @@ command_list = (
             )
         ],
         "outputs": [("tests/assets/testaudio-1m.mp3", None)],
-        "global_options": {"n": None},
+        "global_options": {},
     },
     {
         "inputs": [
@@ -35,19 +35,20 @@ command_list = (
             ("anoisesrc=d=60:c=pink:r=44100:a=0.5:d=60", {"f": "lavfi"}),
         ],
         "outputs": [("tests/assets/testmulti-1m.mp4", {"map": (0, 1, 2, 3)})],
-        "global_options": {"n": None},
+        "global_options": {},
     },
     {
         "inputs": [("testsrc=r=1:d=5", {"f": "lavfi"})],
         "outputs": [("tests/assets/imgs/testimage-%d.png", None)],
-        "global_options": {"n": None},
+        "global_options": {},
     },
 )
 
+overwrite = True
 
 for cfg in command_list:
     url = cfg["outputs"][0][0]
-    if not path.isfile(url):
-        ffmpegprocess.run(cfg)
+    if overwrite or not path.isfile(url):
+        ffmpegprocess.run(cfg, overwrite=overwrite)
     url = url.replace("%d", "1")
     pprint(probe.full_details(url))
